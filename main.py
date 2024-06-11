ee9f608fd1bc87e03b5b444ec1242af118a503d3
James Chase <gochases@gmail.com>
2024-06-10 21:42:30 -0600

    Add main

---
+++ main.py
@@ -0,0 +1,24 @@
+################################################################################
+# Name: James A. Chase
+# File: main.py
+# Date: 10 June 2024
+# Description:
+#
+# Main file
+#
+################################################################################
+
+def main() -> None:
+    """
+    Main function
+
+    :parameter: None
+
+    :return: None
+    """
+    # do nothing
+    pass
+
+
+if __name__ == '__main__':
+    main()
