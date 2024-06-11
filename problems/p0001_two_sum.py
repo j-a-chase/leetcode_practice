a3190214904d180bc781ed187f2f7bdd7b08c8a1
James Chase <gochases@gmail.com>
2024-06-10 22:17:42 -0600

    Add final solution for two_sum

--- problems/p0001_two_sum.py
+++ problems/p0001_two_sum.py
@@ -2,22 +2,18 @@
 # James A. Chase
 # 061024
 #
-from typing import List
+from typing import List, Dict
 
 
 class Solution:
     @staticmethod
     def two_sum(nums: List[int], target: int) -> List[int]:
-        l: int = 0
-        r: int = len(nums) - 1
-        while l < r:
-            if nums[l] + nums[r] == target:
-                return [l, r]
-            elif nums[l] + nums[r] < target:
-                l += 1
-            else:
-                r -= 1
+        m: Dict[int, int] = {}
+        for idx, num in enumerate(nums):
+            if target - num in m:
+                return [m[target - num], idx]
+            m[num] = idx
 
 
 if __name__ == '__main__':
-    solution = Solution()
+    assert False, 'This is a class file. Import its contents into another file.'
